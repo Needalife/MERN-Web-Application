2c99c9566e701cedbf57faae411106f9be91fd0f
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-02 20:09:03 +0700

    Setting up fe

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
