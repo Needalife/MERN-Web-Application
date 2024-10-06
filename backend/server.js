db2d7191d7c0fdef9e60d399c1dd3f329a9c8cca
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-06 11:20:51 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,22 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.length("*", (req,res) => {
-    res.sendFile(path,resolve(__dirname,"frontend", "dist", "index.html"));
-  });
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-app.listen(port, () => {
-  connectDB();
-  console.log(`Server started at http://localhost:${port}`);
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
